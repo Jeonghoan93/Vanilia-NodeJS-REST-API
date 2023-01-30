36f99c771748b6214559abe629b905397048345f
Jeonghoan <zldwkd93@gmail.com>
2023-01-30 18:40:25 +0100

    Merge changes

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,7 @@
 const products = require('../data/products');
+const { v4: uuidv4 } = require('uuid');
+
+const { writeDataToFile } = require('../utils');
 
 function findAll() {
     return new Promise((resolve, reject) => {
@@ -6,11 +9,37 @@ function findAll() {
     })
 }
 
-function findById() {
+function findById(id) {
     return new Promise((resolve, reject) => {
         const product = products.find((p) => p.id === id);
         resolve(product);
     })
 }
 
-module.exports = { findAll, findById };
+function create(product) {
+    return new Promise((resolve, reject) => {
+       const newProduct = { id: uuidv4(), ...product };
+       products.push(newProduct);
+       writeDataToFile('./data/products.json', products);
+       resolve(newProduct);
+    })
+}
+
+function update(id, product) {
+    return new Promise((resolve, reject) => {
+        const index = products.findIndex((p) => p.id === id);
+        products[index] = { id, ...product };
+        writeDataToFile('./data/products.json', products);
+        resolve(products[index]);
+    })
+}
+
+function remove(id) {
+    return new Promise((resolve, reject) => {
+        const index = products.filter((p) => p.id !== id);
+        writeDataToFile('./data/products.json', products);
+        resolve();
+    })
+}
+
+module.exports = { findAll, findById, create, update, remove };
