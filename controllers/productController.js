37cdf517dc54208b777ba5b52a1e4dd30497795c
Jeonghoan <zldwkd93@gmail.com>
2023-01-30 14:15:38 +0100

    nodejs-rest-api

---
+++ controllers/productController.js
@@ -0,0 +1,33 @@
+const Product = require('../models/productModel');
+
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll();
+    
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(products));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// @Dec Gets single Products
+// @route GET /api/products
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if(!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Product not found' }));  
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(product));
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { getProducts, getProduct };
